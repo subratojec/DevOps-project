6018e6b8f1e8dce5525aeb7afa47116c9cff0196
Subrato Singh <kaatib.singh@gmail.com>
2025-09-20 17:27:30 +0530

    newer image

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "It's a Flask appliation running on Pod!"
+    return "It's a Flask appliation running on Pod! and a new version. "
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5001, debug=False)
